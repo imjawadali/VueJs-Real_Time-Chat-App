4b6778c44c871c78c32f48c3cdb958d0485787ab
ahad62426 <AHADS62426@GMAIL.COM>
2019-08-17 17:47:54 +0500

    un necessary assets removed

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <template>
   <div id="app">
-    <Navbar />
     <router-view />
   </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar'
-
 export default {
-  name: 'app',
-  components: {
-    Navbar
-  }
+  name: 'app'
 }
 </script>
 
